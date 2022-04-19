a75b0f847d0eca48b66fc3a5ed4c630652da9de2
reznikovas <ramdirt@mail.ru>
2022-04-19 17:01:30 +0300

    update lesson 4

--- 4/views/basket/index.php
+++ 4/views/basket/index.php
@@ -3,7 +3,7 @@
 <?php foreach ($basket as $product) : ?>
 <div style="background-color: gray; color: white; width: 300px; padding: 20px;">
 
-    <img width="200" src="../assets/img/big/<?= $product->img ?>" alt="">
+    <img width="200" src="./assets/img/big/<?= $product->img ?>" alt="">
     <h3><?= $product->title ?></h3>
     <p>Стоимость: <?= $product->price ?> $</p>
     <p>Количество: <?= $product->quantity ?></p>
--- 4/views/catalog/index.php
+++ 4/views/catalog/index.php
@@ -2,6 +2,8 @@
 
 <?php foreach ($catalog as $item) : ?>
 <div>
+
+    <img width="200" src="./assets/img/big/<?= $item['img'] ?>" alt="">
     <h3><a href="./?c=product&a=index&id=<?= $item['id'] ?>"><?= $item['title'] ?></a></h3>
     <p>price: <?= $item['price'] ?></p>
     <form action="./?c=basket&a=insert&id=<?= $item['id'] ?>" method="post">
